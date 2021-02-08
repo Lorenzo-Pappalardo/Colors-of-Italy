52b2fece1fd2ff7cb4940f8495ebefffa11143b0
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-02-08 13:16:01 +0100

    Scraping works

--- src/scraper.ts
+++ src/scraper.ts
@@ -2,25 +2,20 @@ import * as cheerio from 'cheerio';
 import axios from 'axios';
 import * as fs from 'fs';
 
-export default async function scrape(): Promise<string> {
+//scrape(); // Uncomment when running this single file
+
+export default async function scrape(): Promise<string[]> {
   const AxiosInstance = axios.create();
-  let news: string = 'Error!';
+  let news: string[] = ['Error!'];
 
   await AxiosInstance.get('https://www.unict.it/it/news?page=0').then(res => {
-    const $ = cheerio.load(res.data);
+    const $: cheerio.Root = cheerio.load(res.data);
 
-    let data = $('div[class="content"] > div > div > div');
-    let contents: string[] = [];
-    data.each(function () {
-      contents.push($(this).text());
-    });
+    let data: cheerio.Cheerio = $('div[class=view-content]');
 
-    /* toFile(contents.join());
-      console.log('File has been saved!'); */
+    news = [data.text()];
 
-    news = contents.join(
-      '\n------------------------------------------------------------------------\n'
-    );
+    //toFile(news.join('\n\n'));
   });
   return news;
 }
@@ -29,4 +24,5 @@ function toFile(text: string) {
   fs.writeFile('res.txt', text, error => {
     if (error) throw error;
   });
+  console.log('File has been saved!');
 }
