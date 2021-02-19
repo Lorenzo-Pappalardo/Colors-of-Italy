3533bd5f897eb21d4f4c718e99a44bea5dfb4f42
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-02-19 18:46:05 +0100

    Now the bot is able to show the user a list of italian regions grouped by their respective areas based on the pandemic situation

--- src/scraper.ts
+++ src/scraper.ts
@@ -2,22 +2,9 @@ import * as cheerio from 'cheerio';
 import axios from 'axios';
 import * as fs from 'fs';
 
-//scrape(); // Uncomment when running this single file
-
-export default async function scrape(): Promise<string[]> {
-  const AxiosInstance = axios.create();
-  let news: string[] = ['Error!'];
-
-  await AxiosInstance.get('https://www.unict.it/it/news?page=0').then(res => {
-    const $: cheerio.Root = cheerio.load(res.data);
-
-    let data: cheerio.Cheerio = $('div[class=view-content]');
-
-    news = [data.text()];
-
-    //toFile(news.join('\n\n'));
-  });
-  return news;
+interface Area {
+  color: string;
+  regions: string[];
 }
 
 function toFile(text: string) {
@@ -26,3 +13,59 @@ function toFile(text: string) {
   });
   console.log('File has been saved!');
 }
+
+export default async function scrape(): Promise<string> {
+  const AxiosInstance = axios.create();
+
+  const areas: Area[] = [
+    { color: 'Rossa', regions: [] },
+    { color: 'Arancione', regions: [] },
+    { color: 'Gialla', regions: [] },
+    { color: 'Bianca', regions: [] },
+  ];
+
+  await AxiosInstance.get(
+    'http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=italiano&id=5351&area=nuovoCoronavirus&menu=vuoto'
+  ).then(res => {
+    const $: cheerio.Root = cheerio.load(res.data);
+    let color = -1;
+
+    const data: cheerio.Cheerio = $('div[class=col-md-3]').children();
+    data.each((index: number, element: cheerio.Element) => {
+      if ($(element).text().startsWith('document.write(area')) {
+        color += 1;
+      } else if ($(element).text() !== 'document.write(noRegione);') {
+        const area: Area = areas[color];
+        const regions: string[] | undefined = $(element).html()?.split('<br>');
+        regions?.forEach((value: string) => {
+          if (value !== '') {
+            area.regions.push(value);
+          }
+        });
+      }
+    });
+  });
+
+  // toFile(areas.join('\n\n'));
+
+  let finalResult: string = '';
+  areas.forEach((area: Area) => {
+    finalResult += 'Zona ';
+    finalResult += area.color;
+    finalResult += '\n-------------------\n';
+    if (area.regions.length === 0) {
+      finalResult += 'Nessuna Regione\n\n';
+    }
+    area.regions.forEach((region: string) => {
+      finalResult += region;
+      finalResult += '\n';
+    });
+    finalResult += '\n';
+  });
+
+  return finalResult;
+}
+
+scrape().catch(err => {
+  console.error(err);
+}); // Uncomment when running this single file
