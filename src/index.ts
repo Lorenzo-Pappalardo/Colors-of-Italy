3d2a3e22652c0c3852e2cfe082981c8949bf1752
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2021-10-07 10:50:15 +0200

    Now using token environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,38 @@
 import { Context, Telegraf } from 'telegraf';
 import scrape from './scraper';
 
-const config = require('../config.json');
+const token = process.env.TOKEN || 'UNDEFINED';
 
-const bot: Telegraf<Context> = new Telegraf(config.BOT_TOKEN);
+if (token === 'UNDEFINED') {
+  console.log("You must define a 'TOKEN' environment variable that holds the bot's token");
+} else {
+  const bot: Telegraf<Context> = new Telegraf(token);
 
-bot.use(async (ctx: Context, next: Function) => {
-  const start: Date = new Date();
-  await next();
-  const ms = new Date().getTime() - start.getTime();
-  console.log('Response time: %sms', ms);
-});
+  bot.use(async (ctx: Context, next: Function) => {
+    const start: Date = new Date();
+    await next();
+    const ms = new Date().getTime() - start.getTime();
+    console.log('Response time: %sms', ms);
+  });
 
-bot.start(ctx =>
-  ctx.replyWithSticker('CAACAgIAAxkBAAIClV_g6QGPC23_jVb-FMy_YWbJ_XlEAAJvAAPb234AAZlbUKh7k4B0HgQ')
-);
+  bot.start(ctx =>
+    ctx.replyWithSticker('CAACAgIAAxkBAAIClV_g6QGPC23_jVb-FMy_YWbJ_XlEAAJvAAPb234AAZlbUKh7k4B0HgQ')
+  );
 
-bot.hears(['Hi', 'hi', 'Hello', 'hello'], ctx => ctx.reply('Hey xD'));
+  bot.hears(['Ciao', 'Hi', 'hi', 'Hello', 'hello'], ctx => ctx.reply('Hey xD'));
 
-bot.command('regioni', async ctx =>
-  scrape()
-    .then(res => ctx.replyWithMarkdownV2(res))
-    .catch(err => console.error(err))
-);
+  bot.command('regioni', async ctx =>
+    scrape()
+      .then(res => ctx.replyWithMarkdownV2(res))
+      .catch(err => console.error(err))
+  );
 
-bot.launch().catch(err => {
-  console.error(err);
-});
+  bot.launch().catch(err => {
+    console.error(err);
+  });
 
-console.log('Bot started!');
+  console.log('Bot started!');
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
